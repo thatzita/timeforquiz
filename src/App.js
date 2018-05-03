b660f1dd3f3d46165e457e3a9a6e7d14cccbecee
thatzita <robinudehn@gmail.com>
2018-05-03 19:58:02 +0200

    thatzita

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
     this.state = {
       user: null,
       nickname: '',
-      something: ''
+      email: '',
+      uid: ''
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.login = this.login.bind(this);
+    this.loginGoogle = this.loginGoogle.bind(this);
     this.logout = this.logout.bind(this);
     this.loginFb = this.loginFb.bind(this);
   }
@@ -25,63 +26,70 @@ class App extends Component {
     e.preventDefault();
     const userRef = firebase.database().ref('users');
     const currentUser = {
-      user: null,
       nickname: this.state.nickname,
-      something: this.state.something
+      email: this.state.email
     }
     userRef.push(currentUser);
-    this.setState({nickname: '', something: ''});
+    this.setState({nickname: '', email: ''});
   }
   logout() {
-    auth.signOut()
-  .then(() => {
-    this.setState({
-      user: null
+    auth.signOut().then(() => {
+      this.setState({user: null});
     });
-  });
   }
-  login() {
-    auth.signInWithPopup(provider)
-    .then((result) => {
-      console.log(result)
+  loginGoogle() {
+    auth.signInWithPopup(provider).then((result) => {
+      const userRef = firebase.database().ref('users/');
       const user = result.user;
+      let currentlyLoggedIn = firebase.auth().currentUser;
+
+      let currentUser = {
+        nickname: user.displayName,
+        email: user.email,
+        uid: user.uid
+      }
+      
+      userRef.once("value", function(snapshot) {
+        let data = snapshot.val();
+        let keys = Object.keys(data);
+        // console.log(keys)
+        // console.log(data)
+
+      });
       this.setState({user});
     })
   }
-
-  loginFb(){
-    auth.signInWithPopup(authfb)
-    .then((result) =>{
-
+  loginFb() {
+    auth.signInWithPopup(authfb).then((result) => {
       console.log(result)
-
     })
   }
-    render(){
-      return (<div className="App">
-        <div className="wrapper">
-          <h1>Johan is love, Johan is life</h1>
-          {
-            this.state.user
-              ? <button onClick={this.logout}>Leave Johan</button>
-              : <button onClick={this.login}>Enter Johan</button>
-          }
-        </div>
-
-        <button onClick={this.loginFb}>Enter with facebook</button>
-        <div className='container'>
-          <section className='add-item'>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="nickname" placeholder="thatzita" onChange={this.handleChange} value={this.state.nickname}/>
-              <input type="text" name="something" placeholder="that's it isn't it?" onChange={this.handleChange} value={this.state.something}/>
-              <button >Add it</button>
-            </form>
-          </section>
-        </div>
-      </div>)
-    }
+  render() {
+    return (<div className="App">
+      <div className="wrapper">
+        <h1>Johan is love, Johan is life</h1>
+        {
+          this.state.user
+            ? <button onClick={this.logout}>Leave Johan</button>
+            : <button onClick={this.loginGoogle}>Enter Johan</button>
+        }
+        {
+          this.state.user
+            ? <button onClick={this.logout}>Leave facebook</button>
+            : <button onClick={this.loginFb}>Enter with facebook</button>
+        }
+      </div>
+      <div className='container'>
+        <section className='add-item'>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="nickname" placeholder="thatzita" onChange={this.handleChange} value={this.state.nickname}/>
+            <input type="text" name="email" placeholder="that's it isn't it?" onChange={this.handleChange} value={this.state.email}/>
+            <button >Add it</button>
+          </form>
+        </section>
+      </div>
+    </div>)
   }
-
-
+}
 
 export default App
