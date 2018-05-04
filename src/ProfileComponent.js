9bea800695b0cd569e9eece377caa4b979c4bdd0
thatzita <robinudehn@gmail.com>
2018-05-04 11:08:01 +0200

    hejsan

---
+++ src/ProfileComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class ProfileComponent extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (<div>This will be starting page</div>)
+  }
+}
+
+export default ProfileComponent;
