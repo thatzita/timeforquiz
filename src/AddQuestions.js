1be719e9a05ec676d6473267f79048964bdb7f0d
thatzita <robinudehn@gmail.com>
2018-05-11 13:13:48 +0200

    saker

--- src/AddQuestions.js
+++ src/AddQuestions.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
 import firebase from './firebase.js';
 import SportQuestions from './sportQuestions.js';
+import './AddQuestions.css'
 class AddQuestions extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
       goBack: true,
-      answers: {},
-      question: {},
-      correctAnswer: {},
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      question: '',
+      correctAnswer: '',
       sendMessage: '',
       divId: ''
-
     }
-      console.log(this.state)
+    console.log(this.props.profile)
   }
 
   goBack = () => {
-    console.log(this.state.goBack)
     this.setState({goBack: false})
-
   }
 
   handleChangeQuestion = (e) => {
@@ -29,34 +30,47 @@ class AddQuestions extends Component {
 
   handleChangeA = (e) => {
     this.setState({a: e.target.value})
-
   }
 
   handleChangeB = (e) => {
     this.setState({b: e.target.value})
-
   }
 
   handleChangeC = (e) => {
     this.setState({c: e.target.value})
-
   }
 
   handleChangeD = (e) => {
     this.setState({d: e.target.value})
-
   }
 
-  correctAnswer = (event) => {
-    this.setState({correctAnswer: event.target.value})
-
+  correctAnswer = (e) => {
+    this.setState({correctAnswer: e.target.value})
   }
 
+  componentDidMount() {
+        let btnSend = document.getElementById("btnSend");
+        btnSend.disabled = true;
+  }
+
+  componentDidUpdate() {
+     let btnSend = document.getElementById("btnSend")
+     console.log(btnSend)
+     if(btnSend === null) {
+       console.log("you will come here")
+
+     }else{
+
+     btnSend.disabled = true;
+         if(this.state.question !== '' && this.state.correctAnswer !== '' &&
+           this.state.a !== '' && this.state.b !== '' && this.state.c !== '' && this.state.d !== '') {
+         btnSend.disabled = false
+       }
+     }
+     }
   sendQuestion = () => {
-      
-      let self = this;
-      let form = document.getElementById("theForm");
-
+    let self = this;
+    let form = document.getElementById("theForm");
     firebase.database().ref('questions/genre/sport/').push({
       Question: this.state.question,
       answers: {
@@ -68,44 +82,50 @@ class AddQuestions extends Component {
       correctAnswer: this.state.correctAnswer
     })
 
-     form.reset();
-    
-      this.setState({
-          sendMessage: 'Your question have been sent, sir!',
-          divId: 'itHasBeenSent'
-      })
+    form.reset();
 
-      setInterval(function(){
-        self.setState({
-          sendMessage: '',
-          divId: ''
-          }) 
-      }, 4000);
+    this.setState({sendMessage: 'Your question have been sent, sir!', divId: 'itHasBeenSent'})
+
+    setInterval(function() {
+      self.setState({sendMessage: '', divId: ''})
+    }, 4000);
+
+    this.setState({
+    question: '',
+    a: '',
+    b: '',
+    c: '',
+    d: '',
+    correctAnswer: ''
+  })
   }
 
   render() {
 
     if (!this.state.goBack) {
       return (<div>
-        <SportQuestions/>
+        <SportQuestions profile={this.props.profile}/>
       </div>)
     }
 
-    return (<div>Create your own sport Question!
+    return (<div>
+      <h2>Create your own sport Question!</h2>
       <br/>
-      <button onClick={this.goBack}>Back to SportPage</button>
 
-      <form id="theForm">
-        <br/>What sport question do you want to add?
-        <input type="text" onChange={this.handleChangeQuestion}/>
-        <br/>Answer A:<label><input type="text" onChange={this.handleChangeA}/><input type="radio" id="radioButton" value="a" name="chooseOne" onClick={this.correctAnswer}/></label>
-        <br/>Answer B:<label><input type="text" onChange={this.handleChangeB}/><input type="radio" id="radioButton" value="b" name="chooseOne" onClick={this.correctAnswer}/></label>
-        <br/>Answer C:<label><input type="text" onChange={this.handleChangeC}/><input type="radio" id="radioButton" value="c" name="chooseOne" onClick={this.correctAnswer}/></label>
-        <br/>Answer D:<label><input type="text" onChange={this.handleChangeD}/><input type="radio" id="radioButton" value="d" name="chooseOne" onClick={this.correctAnswer}/></label>
-      </form>
-
+      <div>
+        <form id="theForm">
+          <h3>What sport question do you want to add?
+          </h3><br/>
+          <input className="questionInput" type="text" onChange={this.handleChangeQuestion}/>
+          <br/>Answer A:<label><input className="answerInput" type="text" onChange={this.handleChangeA}/><input type="radio" className="radioButton" value="a" name="chooseOne" onClick={this.correctAnswer}/></label>
+          <br/>Answer B:<label><input className="answerInput" type="text" onChange={this.handleChangeB}/><input type="radio" className="radioButton" value="b" name="chooseOne" onClick={this.correctAnswer}/></label>
+          <br/>Answer C:<label><input className="answerInput" type="text" onChange={this.handleChangeC}/><input type="radio" className="radioButton" value="c" name="chooseOne" onClick={this.correctAnswer}/></label>
+          <br/>Answer D:<label><input className="answerInput" type="text" onChange={this.handleChangeD}/><input type="radio" className="radioButton" value="d" name="chooseOne" onClick={this.correctAnswer}/></label>
+        </form>
+      </div>
       <br/>
-      <button onClick={this.sendQuestion}>Send Question!</button>
+      <button id="btnSend" className="btnQ" onClick={this.sendQuestion}>Send Question!</button>
+      <button className="btnQ" onClick={this.goBack} >Back to SportPage</button>
       <div id={this.state.divId}>{this.state.sendMessage}</div>
     </div>)
   }
